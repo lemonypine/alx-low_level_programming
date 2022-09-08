56acf5ee1199ac7c2dd0bc12010c4e50330818d3
lemonypine <r.wanja.gichuki@gmail.com>
2022-09-08 20:34:00 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always returns 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n ");
+	return (0);
+}
