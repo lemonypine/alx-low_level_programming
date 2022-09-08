c0ad71cb524d442b7db24d839e4b28e34d87c814
lemonypine <r.wanja.gichuki@gmail.com>
2022-09-08 21:44:45 +0000

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - entery point of program
+* 
+* Return: always 0 (success)
+*/
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	printf("\n");
+	return (0);
+}
